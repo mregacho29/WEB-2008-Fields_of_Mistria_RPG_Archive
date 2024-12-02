233dd48dc6f05418d13aae22acd2e10bdec5a4f2
mregacho29 <mregacho@academic.rrc.ca>
2024-12-02 07:40:15 -0600

    Updated index.php and added spacing between carousel and container

--- header.php
+++ header.php
@@ -20,27 +20,13 @@ $current_page = basename($_SERVER['PHP_SELF']);
   </head>
 <body>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Fields of Mistria RPG Archive</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <!-- Google Fonts Roboto -->
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap"
-    />
-</head>
-<body>
 
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <!-- Container wrapper -->
   <div class="container-fluid">
     <!-- Navbar brand -->
-    <a class="navbar-brand me-2" href="#">
+    <a class="navbar-brand me-2" href="index.php">
       <img
         src="image/Fields_of_Mistria_Title.webp"
         height="55"
